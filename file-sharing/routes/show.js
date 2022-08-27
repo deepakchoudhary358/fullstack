6691328f1633ee358587e1e66152626001509ba2
deepakchoudhary358 <deepakchoudhary358@gmail.com>
2022-08-27 17:05:03 +0530

    Download page

---
+++ file-sharing/routes/show.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const File = require('../models/files');
+
+
+router.get('/:uuiid', async (req, res) => {
+    try {
+        const file = await File.findOne({uuid: req.params.uuiid});
+        if(!file) {
+            return res.render('download', { error: 'Something went wrong...'});
+        }
+        return res.render('download', {
+            file: file.uuid,
+            fileName: file.fileName,
+            fileSize: file.filesize,
+            download: `${process.env.APP_BASE_URL}/files/download${file.uuid}`
+        });
+    } catch(error) {
+        return res.render('download', { error: 'Something went wrong...'});
+    }
+    
+});
+
+
+module.exports = router;
--- file-sharing/server.js
+++ file-sharing/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/files');
+const show = require('./routes/show');
 const connectDB = require('./config/db');
+const path = require('path');
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, async () => {
@@ -9,4 +11,7 @@ app.listen(PORT, async () => {
     console.log(`Listening on PORT : ${PORT}`);
 });
 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejb')
 app.use('/api/files', router);
+app.use('/files', show);
